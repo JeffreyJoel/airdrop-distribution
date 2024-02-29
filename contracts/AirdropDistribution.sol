c76db9dbb1b062f2d96f0783046fd0d2c6c4dfde
JeffreyJoel <jowoloko@gmail.com>
2024-02-29 15:43:15 +0100

    update private fuction convention

--- contracts/AirdropDistribution.sol
+++ contracts/AirdropDistribution.sol
@@ -2,10 +2,10 @@
 pragma solidity ^0.8.24;
 
 contract AirdropDistribution {
-
     //Custom errors
     error ADDRESS_ZERO_DETECTED();
     error ADDRESS_ALREADY_EXISTS();
+    error AIRDROP_HAS_ENDED();
 
     struct User {
         uint id;
@@ -16,71 +16,74 @@ contract AirdropDistribution {
         bool hasReachedMinEntryPoint;
     }
 
-    uint trackUserId; 
-    
+    uint trackUserId;
+
     uint8 constant followPoints = 5;
     uint8 constant sharePoints = 5;
     uint8 constant likePoints = 5;
-
     uint constant entryPoint = 10;
+
+    bool isAirdropEnded = false;
 
     mapping(address => User) users;
 
     uint[] participantsId;
 
-    function userAlreadyExists() private view{
-          if(users[msg.sender].isRegistered == true){
-            revert ADDRESS_ALREADY_EXISTS();
-        } 
-    }
+    constructor() {}
 
-     function trackUserPoints() private {
-            if(users[msg.sender].hasReachedMinEntryPoint ){
-            if(users[msg.sender].points >= entryPoint ){
-                users[msg.sender].hasReachedMinEntryPoint = true;
-                participantsId.push(users[msg.sender].id);
-
-//Disburse Code
-                if(participantsId.length == 10){
-                    //handle Disburse
-                }
-            }
-        }
-    }
-
-    constructor() {
-
-    }
-
-    function registerUser() external{
+    function registerUser() external {
         // check address zero;
         //check if the address has been registered
-        if(msg.sender == address(0)){
+        if (isAirdropEnded = true) {
+            revert AIRDROP_HAS_ENDED();
+        }
+        if (msg.sender == address(0)) {
             revert ADDRESS_ZERO_DETECTED();
         }
-        userAlreadyExists();     
+        _userAlreadyExists();
 
         uint _userId = trackUserId + 1;
-        User memory _user =  User(_userId, false, false, false, 0, false);
+        User memory _user = User(_userId, false, false, false, 0, false);
 
         users[msg.sender] = _user;
 
         trackUserId = trackUserId + 1;
     }
 
-     function likePost() external{
-         userAlreadyExists();
-        users[msg.sender].hasLikedPost = true; 
+    function likePost() external {
+        _userAlreadyExists();
+        users[msg.sender].hasLikedPost = true;
         users[msg.sender].points = users[msg.sender].points + likePoints;
-        trackUserPoints();
-
+        _trackUserPoints();
     }
 
-     function follow() external{
-         userAlreadyExists();
-        users[msg.sender].hasFollowed = true; 
+    function follow() external {
+        _userAlreadyExists();
+        users[msg.sender].hasFollowed = true;
         users[msg.sender].points = users[msg.sender].points + followPoints;
-         trackUserPoints();
+        _trackUserPoints();
     }
-    
+
+    function distributeRewards() private {
+        //Disburse Code
+        if (participantsId.length == 10) {
+            //handle Disburse with chainlink vrf and IERC20
+            isAirdropEnded = true;
+        }
+    }
+ 
+    function _userAlreadyExists() private view {
+        if (users[msg.sender].isRegistered == true) {
+            revert ADDRESS_ALREADY_EXISTS();
+        }
+    }
+
+    function _trackUserPoints() private {
+        if (users[msg.sender].hasReachedMinEntryPoint) {
+            if (users[msg.sender].points >= entryPoint) {
+                users[msg.sender].hasReachedMinEntryPoint = true;
+                participantsId.push(users[msg.sender].id);
+            }
+        }
+    }
 }
